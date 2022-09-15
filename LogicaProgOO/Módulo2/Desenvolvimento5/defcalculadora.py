efdd4d92e501f186c0d58b58711da596c4b59cb2
Hesdras Francisco de Morais <108990874+flleuma@users.noreply.github.com>
2022-09-15 16:36:48 -0300

    Update file

---
+++ LogicaProgOO/Módulo2/Desenvolvimento5/defcalculadora.py
@@ -0,0 +1,12 @@
+def calculadora(n1, n2, op):
+  if op == 1:
+    return n1 + n2
+  elif op == 2:
+    return n1 - n2
+  elif op == 3:
+    return n1 * n2
+  elif op == 4:
+    return n1 / n2
+  else:
+    return 0
+
